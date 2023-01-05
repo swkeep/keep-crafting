88077ba9fd2bde527da2a0576bc2d981ff56dffd
swkeep <badaosoft@gmail.com>
2023-01-05 17:29:11 +0330

    updated config file

--- config.lua
+++ config.lua
@@ -349,76 +349,49 @@ Config.workbench_default_model = 'gr_prop_gr_bench_04a'
 
 Config.workbenches = {
      -- -- items
-     -- {
-     --      table_model = "gr_prop_gr_bench_04b",
-     --      coords = vector3(1346.55, 4391.04, 43.36),
-     --      item_show_case_offset = vector3(0.0, 0.0, 1.3),
-     --      rotation = vector3(0.0, 0.0, 350),
-     --      -- just use either job or gang using both at same time won't work.
-     --      job = {
-     --           allowed_list = {},
-     --           allowed_grades = {}
-     --      },
-     --      -- gang = {
-     --      --      allowed_list = { 'ballas' },
-     --      --      allowed_grades = { ['ballas'] = { 1, 2, 3, 4 } }
-     --      -- },
-     --      categories = { Config.categories.misc, Config.categories.medical },
-     --      recipes = { misc_recipe, medial },
-     --      radius = 3.0
-     -- },
-     -- {
-     --      table_model = 'gr_prop_gr_bench_04b',
-     --      coords = vector3(-59.5, 6388.77, 30.49),
-     --      rotation = vector3(0.0, 0.0, 225.0),
-     --      job = {
-     --           allowed_list = {},
-     --           allowed_grades = {}
-     --      },
-     --      categories = { Config.categories.misc, Config.categories.medical },
-     --      recipes = { misc_recipe, medial },
-     --      radius = 3.0
-     -- },
-     -- gun
-     -- { --
-     --      table_model = 'gr_prop_gr_bench_02b',
-     --      coords = vector3(-891.61, -1444.84, 6.53),
-     --      item_show_case_offset = vector3(0.0, 0.0, 1.2),
-     --      rotation = vector3(0.0, 0.0, 200.0),
-     --      job = {
-     --           allowed_list = {},
-     --           allowed_grades = {}
-     --      },
-     --      categories = { Config.categories.weapons, Config.categories.misc, Config.categories.medical },
-     --      recipes = { weapons_recipe, misc_recipe, medial },
-     --      radius = 3.0
-     -- },
      {
-          table_model = 'gr_prop_gr_bench_02b',
-          coords = vector3(407.5, -1612.91, 28.29),
-          item_show_case_offset = vector3(0.0, 0.0, 1.2),
-          rotation = vector3(0.0, 0.0, 90.0),
+          table_model = "gr_prop_gr_bench_04b",
+          coords = vector3(1346.55, 4391.04, 43.36),
+          item_show_case_offset = vector3(0.0, 0.0, 1.3),
+          rotation = vector3(0.0, 0.0, 350),
+          -- just use either job or gang using both at same time won't work.
           job = {
                allowed_list = {},
                allowed_grades = {}
           },
-          categories = { Config.categories.blueprints, Config.categories.weapons, Config.categories.misc, Config.categories.medical },
-          recipes = { weapons_recipe, misc_recipe, medial },
+          -- gang = {
+          --      allowed_list = { 'ballas' },
+          --      allowed_grades = { ['ballas'] = { 1, 2, 3, 4 } }
+          -- },
+          categories = { Config.categories.blueprints, Config.categories.misc, Config.categories.medical },
+          recipes = { misc_recipe, medial },
           radius = 3.0
      },
-     -- {
-     --      table_model = 'gr_prop_gr_bench_02b',
-     --      coords = vector3(2939.04, 4623.81, 47.72),
-     --      item_show_case_offset = vector3(0.0, 0.0, 1.2),
-     --      rotation = vector3(0.0, 0.0, 47.0),
-     --      gang = {
-     --           allowed_list = { 'ballas' },
-     --           allowed_grades = { ['ballas'] = { 1, 2, 3, 4 } }
-     --      },
-     --      categories = { Config.categories.weapons },
-     --      recipes = { weapons_recipe },
-     --      radius = 3.0
-     -- }
+     {
+          table_model = 'gr_prop_gr_bench_04b',
+          coords = vector3(-59.5, 6388.77, 30.49),
+          rotation = vector3(0.0, 0.0, 225.0),
+          job = {
+               allowed_list = {},
+               allowed_grades = {}
+          },
+          categories = { Config.categories.blueprints, Config.categories.misc, Config.categories.medical },
+          recipes = { misc_recipe, medial },
+          radius = 3.0
+     },
+     {
+          table_model = 'gr_prop_gr_bench_02b',
+          coords = vector3(2939.04, 4623.81, 47.72),
+          item_show_case_offset = vector3(0.0, 0.0, 1.2),
+          rotation = vector3(0.0, 0.0, 47.0),
+          gang = {
+               allowed_list = { 'ballas' },
+               allowed_grades = { ['ballas'] = { 1, 2, 3, 4 } }
+          },
+          categories = { Config.categories.weapons },
+          recipes = { weapons_recipe },
+          radius = 3.0
+     },
 }
 
 --gr_prop_gr_jailer_keys_01a
